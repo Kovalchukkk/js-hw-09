55efac127f15c5e7ea0c9aa6a8100bbe082346a4
Kovalchukkk <71971559+Kovalchukkk@users.noreply.github.com>
2022-08-13 13:52:27 +0000

    Deploying to gh-pages from @ Kovalchukkk/js-hw-09@505ced687e01cb7fb6d4a79333136a9db7be9d8b 🚀

---
+++ 01-color-switcher.0a50ae46.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[data-stop]"),document.body;
+//# sourceMappingURL=01-color-switcher.0a50ae46.js.map
---
+++ 01-color-switcher.d8662363.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[data-stop]"),document.body;
+//# sourceMappingURL=01-color-switcher.d8662363.js.map
