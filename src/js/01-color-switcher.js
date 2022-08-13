f8e3e5f89067b78f4db695d02deef27bfde4617d
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-13 16:52:13 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,3 +3,7 @@ const refs = {
   stopBtn: document.querySelector('button[data-stop]'),
   body: document.body,
 };
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
