8b3dfe0e266d6ba3202abc48a7c0343d71c04539
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-13 17:18:41 +0300

    add click handler

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,6 +4,15 @@ const refs = {
   body: document.body,
 };
 
+let timerId = null;
+
+refs.startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.setAttribute('disabled', true);
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
