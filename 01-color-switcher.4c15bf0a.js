bd4a1bd0b444ea2962dd3cd8e562747e89d4039a
Kovalchukkk <71971559+Kovalchukkk@users.noreply.github.com>
2022-08-13 14:35:02 +0000

    Deploying to gh-pages from @ Kovalchukkk/js-hw-09@717983ef46b697023e6cb6f8e250d782f144a1a5 🚀

---
+++ 01-color-switcher.4c15bf0a.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.body};let e=null;t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.setAttribute("disabled",!0)})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.4c15bf0a.js.map
