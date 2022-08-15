bb60445fbc44b2296ebbe350f8f7b75009e45321
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-15 16:50:03 +0300

    add refs

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,10 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
