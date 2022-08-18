100efbec4f2bf7ee2ccb963ee33bfac82de79fcd
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-18 19:12:09 +0300

    add formData

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,6 +6,18 @@ const refs = {
 
 refs.form.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const formData = {};
+  const formDataRaw = new FormData(e.currentTarget);
+
+  formDataRaw.forEach((value, name) => {
+    formData[name] = value;
+  });
+  console.log(formData);
+}
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
